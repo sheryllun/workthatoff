bf828e45e56c5ee374c6e2dee28c3b283f1d38e9
sheryllun <sheryl.v.lun@gmail.com>
2016-07-02 15:43:54 -0400

    Set up configurations and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,24 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect'); //run a local web server
 var open = require('gulp-open'); //open a url in a web browser
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var babelify = require('babelify');
+var concat = require('gulp-concat');
+var lint = require('gulp-eslint');
 
 var config = {
 	port: 9005,
 	devBaseUrl: 'http://localhost',
 	paths: {
 		html: './src/*.html',
+		js: './src/**/*.js',
+		css: [
+			'node_modules/bootstrap/dist/css/bootstrap.min.css',
+			'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+		],
 		dist: './dist',
+		mainJs: './src/main.js'
 	}
 };
 
@@ -33,8 +44,32 @@ gulp.task('html', function() {
 		.pipe(connect.reload());
 });
 
-gulp.task('watch', function() {
-	gulp.watch(config.paths.html, ['html']);
+gulp.task('js', function() {
+	browserify(config.paths.mainJs)
+		.transform(babelify, { presets: ['react']})
+		.bundle()
+		.on('error', console.error.bind(console))
+		.pipe(source('bundle.js'))
+		.pipe(gulp.dest(config.paths.dist + '/scripts'))
+		.pipe(connect.reload());
 });
 
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('css', function() {
+	gulp.src(config.paths.css)
+		.pipe(concat('bundle.css'))
+		.pipe(gulp.dest(config.paths.dist + '/css'));
+});
+
+gulp.task('lint', function() {
+	return gulp.src(config.paths.js)
+		.pipe(lint())
+		.pipe(lint.format());
+});
+
+gulp.task('watch', function() {
+	gulp.watch(config.paths.html, ['html']);
+	gulp.watch(config.paths.js, ['js', 'lint']);
+
+});
+
+gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']);
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+$ = jQuery = require('jquery'); //requiring jquery in the global namespace for bootstrap
 var App = console.log('hello from browserify');
+
 
 module.exports = App;
